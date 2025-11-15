a4fd86481fc27ab142e1e912ad108f180bf1e0d1
Celeste <celestedeschamphelaere@protonmail.com>
2025-11-15 12:42:49 +0100

    not from pretrained

--- SAE_bias_analysis.py
+++ SAE_bias_analysis.py
@@ -33,7 +33,7 @@ model = AutoModelForCausalLM.from_pretrained(
 
 # Load SAE
 print(f"Loading SAE for layer {LAYER}...")
-sae, cfg_dict, sparsity = SAE.from_pretrained(
+sae, cfg_dict, sparsity = SAE.from_pretrained_with_cfg_and_sparsity(
     release=SAE_RELEASE,
     sae_id=SAE_ID,
     device=DEVICE
